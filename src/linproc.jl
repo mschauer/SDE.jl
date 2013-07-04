14d791c31177ffd2f3332479fac87fc7ec84fc15
mschauer <moritzschauer@web.de>
2013-07-04 23:47:29 +0200

    linproc.jl: extended documentation.

--- src/linproc.jl
+++ src/linproc.jl
@@ -35,45 +35,39 @@ export H, r, p, Bstar, Bcirc, Bsharp
 #%  ---------
 #%
 
+
+#function B(b, beta)
+#	(t,x) -> B*x + beta
+#end	
+
+#function SIG(sigma)
+#	(t,x) -> sigma
+#end	
+
+
+#%  .. function:: mu(h, x, B, beta)
+#%               
+#%  	Expectation :math:`E_x(X_{t})`
+#%  	
+
+function mu(h, x, B, beta)
+	
+	binv = inv(B)
+	phi = expm(h*B)
+	phim = expm(-h*B)
+	integral = binv*beta - phim * binv * beta
+	phi*(x + integral)
+end	
+
 #%  .. function:: K(h, lambda, b)
 #%               
 #%  	Covariance matrix :math:`Cov(X_{t}, X_{t + h})`
 #%  	
 
-function B(b, beta)
-	(t,x) -> B*x + beta
-end	
-
-function SIG(sigma)
-	(t,x) -> sigma
-end	
-
-
 function K(h, b, lambda)
 	phi = expm(h*b)
 	lambda - phi*lambda*phi'
 end
-
-function H(h, b, lambda)
-	phim = expm(-h*b)
-	inv(lambda - phim*lambda*phim')
-end
-
-function Hinv(h, b, lambda)
-	phim = expm(-h*b)
-	lambda - phim*lambda*phim'
-end
-
-
-function V(h, v, b, beta)
-	binv = inv(b)
-	phim = expm(-h*b)
-	phim*v + phim * binv * beta - binv * beta 
-end
-#	binv = inv(B)
-#	phim = expm(-h*B)
-#	vt =  phim*( v) + phim * binv * beta -binv*beta 
-
 
 #%  .. function:: r(h, x, v, b, beta, lambda)
 #%               
@@ -85,6 +79,28 @@ end
 function r(h, x, v, b, beta, lambda)
 	H(h, b, lambda)*(x - V(h, v, b, beta))
 end
+
+
+
+#%  .. function:: K(h, lambda, b)
+#%               
+#%  	Hessian of :math:`\log p(t,x; T, v) as a function of x.
+#%  	
+
+function H(h, b, lambda)
+	phim = expm(-h*b)
+	inv(lambda - phim*lambda*phim')
+end
+
+
+# technical function
+
+function V(h, v, b, beta)
+	binv = inv(b)
+	phim = expm(-h*b)
+	phim*v + phim * binv * beta - binv * beta 
+end
+
 
 #%  .. function:: Bstar(T, v, b, beta, a, lambda)
 #%               
@@ -105,6 +121,7 @@ function Bcirc(T, v, b, sigma, B, beta, lambda)
 end	
 
 
+# alternative proposal process
 
 function Bsharp(T, v, b )
 	(t,x) -> b(t,x) + (v-x)/(T-t)
@@ -119,30 +136,11 @@ function lp(h, x, y, b, beta, lambda)
 	z = (x - V(h, y, b, beta))
 	(-1/2*length(x)*log(2pi) - 0.5*log(det(K(h,b, lambda))) + 0.5*z'*H(h, b, lambda)*z) 
 end
-#%  .. function:: lp0(h, x, y,  mu, gamma)
+
+#%  .. function:: sample_p(h, x, b, beta, lambda) 
 #%               
-#%  	Returns :math:`log p(t,x; T, y)`, the transition density of a Brownian motion with drift mu and diffusion a=inv(gamma), h = T - t 
+#%  	Samples from the transition density of the linear process, h = T - t. 
 #%  
-function lp0(h, x, y, mu, gamma)
-          (-1/2*length(x)*log(2pi*h) + 0.5*log(det(gamma))  -0.5*(y-x-h*mu)'*gamma*(y-x-h*mu)/h)
-         
-end
-
-function sample_p0(h, x, mu, l) #l = chol(a)
-	z = randn(length(x))
-	x + l*z*sqrt(h) + h*mu
-end
-
-
-function mu(h, x, B, beta)
-	
-	binv = inv(B)
-	phi = expm(h*B)
-	phim = expm(-h*B)
-	integral = binv*beta - phim * binv * beta
-	phi*(x + integral)
-end	
-
 
 function sample_p(h, x, b, beta, lambda) 
 	phi = expm(h*b)
@@ -155,6 +153,28 @@ function sample_p(h, x, b, beta, lambda)
 	z = randn(length(x))
 	mu + l*z
 end
+
+
+#%  .. function:: lp0(h, x, y,  mu, gamma)
+#%               
+#%  	Returns :math:`log p(t,x; T, y)`, the log transition density of a Brownian motion with drift mu and diffusion a=inv(gamma), h = T - t 
+#%  
+function lp0(h, x, y, mu, gamma)
+          (-1/2*length(x)*log(2pi*h) + 0.5*log(det(gamma))  -0.5*(y-x-h*mu)'*gamma*(y-x-h*mu)/h)
+         
+end
+#%  .. function:: sample_p0(h, x, mu, l) 
+#%               
+#%  	Samples from the transition density a affine Brownian motion. Takes the Cholesky
+#%  	factor as argument. 
+#%  		l = chol(a)
+#%  
+
+function sample_p0(h, x, mu, l) #l = chol(a)
+	z = randn(length(x))
+	x + l*z*sqrt(h) + h*mu
+end
+
 
 
 end #linproc
