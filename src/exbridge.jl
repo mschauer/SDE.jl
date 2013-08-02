8a424c7396aa88fadd6b65a502712df3bc235d41
mschauer <moritzschauer@web.de>
2013-08-02 18:12:09 +0200

    exbridge.jl: Adapting example to time changed proposals.

--- src/exbridge.jl
+++ src/exbridge.jl
@@ -1,72 +1,50 @@
-using Lyap
+#using Winston
+ 
 #using Diffusion
 require("leading.jl")
 require("linproc.jl")
 require("misc.jl")
+require("lyap.jl")
 srand(3)
  
 SV = false #save images?
 
 
-function ito(x, dy)
-	n = length(dy) + 1
-	y = 0.0
-	for i in 2:n
-		y = y + x[i-1]*dy[i-1] 
+
+si = 0.08
+d = 2
+B = [ -0.2   -1;   0.5  -0.4]
+bB = copy(B)
+#B = [ -2.21225   -0.49590;   0.631753  -1.34795]
+#A = [ 0.170252  0.178533; 0.178533  2.43255]
+
+beta = [0.5,-0.5]
+#beta = [0.,0.]
+
+function sigma0(s,y)
+	x = copy(y) - beta
+	m = norm(x)
+	if (m > eps())  
+		rho = 1 + 5*2atan(m)/pi
+		return( si*(eye(2)+ 2atan(m)/pi/m*[[x[2], -x[1]]  [rho*x[1], rho*x[2]]]))
 	end
-	y
+	return si*eye(2)
 end
+#sigma(s,x) = [[1., -1.]  [5., 5.]]
+sigma(t,x) = sigma0(0., [sin(t), cos(t)])
+#sigma(t,x) = sigma0(t,x)
 
+a(s,x) = sigma(s,x)*sigma(s,x)'
 
-function euler(t0, u, b, sigma, dt, dw::Matrix)
-	S = size(dw)
-	endd = length(S)	
-	N = S[end] + 1
-	
-	shape = size(sigma(0,u)*leading(dw,1))
  
-	X = zeros(shape..., N)
 
-	#delta = copy(u)
-	y = copy(u)
-	t = t0
-
-	
-	for i in 1:N-1
-		subleading(X,i)[:] = y
-		t += dt[i]
-		y[:] = y .+  b(t,y)*(dt[i]) .+ sigma(t,y)*leading(dw, i)
-	
-	end
-	subleading(X,N)[:] = y
-	X
-end
-function eulerv(t0, u, v, b, sigma, dt, dw::Matrix)
-	X = euler(t0, u,  b, sigma, dt, dw)
-	X[:,end] = v
-	X
-end
-
-
-function likelixcirc(t, T, v, Xcirc, b, a,  B, beta, lambda)
-#ignores last value
-	
-	function L(s,x)
-		R = LinProc.H(T-s, B, lambda)*(x - LinProc.V(T-s, v, B, beta))
-	  	return (b(s,x) - B*x - beta)' * R + 0.5 *trace((a(s,x) - a(T,v)) *( LinProc.H(T-s, B, lambda) + R*R'))
-	end
-	
-	sum = 0
-	N = size(Xcirc,2)
-	for i in 0:N-1-1 #skip last value, summing over n-1 elements
-	  s = t + (T-t)*(i)/(N-1) 
-	  x = leading(Xcirc, i+1)
-	  sum += scalar(L(s, x)) * (T-t)/(N-1)
-	end
-	
-	exp(sum)
-end
-
+b(t,x) = exp(-0.2*t)*bB*x + beta
+ph(t,s) = 5.*exp(-0.2*s)-5.*exp(-0.2*t)
+#b(t,x) =  atan(norm(x))*B*x + beta
+ 
+u = [1.,0.]
+ 
+ 
 
 
 function plstep(xt, xd, y, yprop)
@@ -74,7 +52,7 @@ function plstep(xt, xd, y, yprop)
 	setattr(p, "xrange", R1)
 	setattr(p, "yrange", R2)
 
-	x = apply(hcat, y)
+ 	x = apply(hcat, y)
 	xprop = apply(hcat, yprop)
 	
 	add(p, Curve(xt[1,:],xt[2,:], "color","grey"))
@@ -87,190 +65,189 @@ function plstep(xt, xd, y, yprop)
 	 
 	p
 end
+function plstep(p, xt, xd, yprop, ll, llmax, m)
+	setattr(p, "xrange", R1)
+	setattr(p, "yrange", R2)
 
+ 	xprop = yprop[m]
+	
+	#add(p, Curve(xt[1,:],xt[2,:], "color","grey"))
+	add(p, Curve(xprop[1,:],xprop[2,:], "color","black", "linewidth", 0.7*min(5.,(ll[m]/llmax[m]))))
+	 
+	add(p, Points(xd[1,m:(m+1)],xd[2,m:(m+1)],"symboltype", "filled circle", "color", "red", "size", 1.2))
+	add(p, Points(xd[1,m:(m+1)],xd[2,m:(m+1)],"symboltype", "empty circle", "color", "white", "size", 0.6))
+	 
+	p
+end
 function plobs(xt, xd)
 	p = FramedPlot()
 	setattr(p, "xrange", R1)
 	setattr(p, "yrange", R2)
 	
-	add(p, Curve(xt[1,:],xt[2,:], "color","black"))
-	add(p, Points(xd[1,:],xd[2,:],"type", "filled circle", "color","red"))
+	add(p, Curve(xt[1,:],xt[2,:], "color","black", "linewidth", 0.5))
+	add(p, Points(xd[1,:],xd[2,:],"symboltype", "filled circle", "color", "red", "size", 1.2))
+	add(p, Points(xd[1,:],xd[2,:],"symboltype", "empty circle", "color", "white", "size", 0.6))
 	
 	Winston.display(p)
 	 
 	p
 end
-
  
-
-#th = 1.7
-#b has root/focus at [-sqrt(th), 0]
-#linearization of (th - x[1]*x[1])*x[1] in -sqrt(th): y  =  -2*th*x[1]-2*th^(3/2)
-th = 1.3
-si = 0.1
-# si = 0.05
-u = [-sqrt(th), -0.5] # start below focus
-d = 2
-zd = zeros(d)
-od = ones(d)
-Id = eye(d)
-
-b(s,x) = [x[2], -x[2] + (th - x[1]*x[1])*x[1]]
-#Lb(s, x) = [x[2], -x[2] - 2*th*x[1] - 2*th^(3/2)] 
-
-B = [0 1; -2*th -1]
-beta = [0, -2*th^(3/2)]
-
-function sigma(s,y)
-	x = copy(y) - [-sqrt(th),0]
-	m = norm(x)
-	if (m <= eps()) return(sqrt(2.0) * 0.5 .* [[1. -1.],[1. 1.]]) end
-	rho = 1 + 5*atan(m)
-	si/m*[[x[2], -x[1]]  [rho*x[1], rho*x[2]]]
-end
-#sigma(s,x) = si*eye(2)
-
-a= (s,x) -> sigma(s,x)*sigma(s,x)'
-
 
 
 print("Generate x")
-
+ 
+###
+M = 4 #number of bridges
+n = 1000 #samples each bridge
 N = 12000 + 1   #full observations
-T = 5		#time span
-T = 4
-Dt = diff(linspace(0., T, N))
+TT = 0.5*M		#time span
+ 
+Dt = diff(linspace(0., TT, N))
 dt = Dt[1]
 DW = randn(2, N-1) .* sqrt(dt)
-x = euler(0.0, u, b, sigma, Dt, DW)
+x = LinProc.eulerv(0.0, u, b, sigma, Dt, DW)
 
 	
 
 #compute range of observations
 R1 = range(x[1,:]) 
- 
-R1 = (R1[1] -0.1*(R1[2]-R1[1]),R1[2] + 0.1*(R1[2]-R1[1]))
+mar = 0.5
+R1 = (R1[1] -mar*(R1[2]-R1[1]),R1[2] + mar*(R1[2]-R1[1]))
+R2 = range(x[2,:]) 
+R2 = (R2[1]  -mar*(R2[2]-R2[1]), R2[2] + mar*(R2[2]-R2[1]))
 
-R2 = range(x[2,:])
-R2 = (R2[1]  -0.1*(R2[2]-R2[1]), R2[2] + 0.1*(R2[2]-R2[1]))
 
-
-#R2 = (-1.5,-1)
-#R2 = (-0.5,0.5)
+#R1 = (-10., 10.)
+#R2 = (-10., 10.)  
 println(".")
 
 
 #Prior variance
-
-s = 20.0
-
-
-
-L(theta, x, Dt) = ito(-x[2, :] + theta*x[1, :] - x[1, :].^3, diff(x[2, :],2))- 0.5*ito( (-x[2, :] + theta*x[1, :] - x[1, :].^3).^2 , Dt)
-
-#mu(x) = ito(x[1, :], diff(x[2, :],2))/(si^2) - ito(x[1, :].*(- x[2, :] - x[1, :].^3), dt)/(si^2)
-mu0(x, Dt) = ito(x[1, :], diff(x[2, :],2))/(si^2) + ito(x[1, :].* x[2, :] + abs(x[1, :]).^4, Dt)/(si^2)
-W0(x, Dt) =  ito(x[1, :].^2, Dt)/(si^2) 
-
-
-
-m = mu0(x, Dt)
-w = W0(x, Dt)  +  1./s^2
-println("th= ", round(m/w,5), "+-", round(1.96*sqrt(1/w), 5))
  
-###
-M = 1 #number of bridges
-n = 1000 #samples each bridge
+
+ 
+
+
 xd = x[:, 1:(N-1)/M:end]
 xtrue = x[:, 1:(N-1)/M/n:end]
 
 p = plobs(x, xd)
-if(SV) Winston.file(p, "img/s$(si)obs.png") end
+if(SV) Winston.file(p, "img/obs.png") end
 
-a=(s,x) -> sigma(s,x)*sigma(s,x)'
+#      [1869.16,515.23,170074.054,301.773]
+#k 100    acc 25.0[44.0,26.0,23.0,7.0]
+#bridge acc %25.0[44.0,26.0,23.0,7.0]
+#        [1.976,1.849,1.49,6.175]
+#k 100    acc 75.5[88.0,78.0,77.0,59.0]
+#bridge acc %75.5[88.0,78.0,77.0,59.0]
 
-v = [-sqrt(th), 0.0]
-B = [0 1; -2*th -1]
-beta = [0, -2*th^(3/2)]
-lambda = Lyap.lyap(B', -a(0,v))
-z = euler(0.0, u, v,  LinProc.Bcirc(T, v, b, sigma, B, beta, lambda), sigma, Dt, DW)
-ll = likelixcirc(0.0, T, v, z, b, a, B, beta, lambda)
-stop()
 #println(ll)
-#error()
+error()
 
 #th = 3/4*th
 yprop = cell(M)
 y = cell(M)
+U = zeros(2,n+1)
+llold = zeros(M)
+ll = zeros(M)
+llmax = zeros(M)
+yprop = cell(M)
+y = cell(M)
 yy = zeros(2,n+1)
 K = 100
-thetas = zeros(K)
-alpha = 1.0 
-
-
+ 
+ S = Smax =  Y = Tmax = Tmin = []
 #accepted bridges
-bb = 0
+bb = zeros(M)
+example = "lin"
+#example = "brown"
+
+p = cell(M)	
+for m = 1:M 
+	p[m] = FramedPlot()
+end
+
+srand(3)
 for k = 1:K
-	b(s,x) = [x[2], -x[2] + (th - x[1]*x[1])*x[1]]
-	th2 = th
-	B = alpha*[0 1; -2*th2 -1]
-	beta = alpha*[0, -2*th2^(3/2)]
-	dt = T/M/n
-	Dt = dt*ones(n-1)
+ 	dt = TT/M/n
+	if (k == K/2)
+		for m = 1:M; p[m] = FramedPlot(); end
+		srand(3)
+	end
 	
 	for m = 1:M
 		u = leading(xd, m)
 		v = leading(xd, m+1)
-	#	println("u -> v $u $v")
-		DW = randn(2,n-1)*sqrt(dt)
-	
-		lambda = Lyap.lyap(B', -a(0,v))
-
 		
-		yy = euler(((m-1)/M)*T, u, LinProc.Bcirc((m/M)*T, v, b, sigma, B, beta, lambda), sigma, Dt, DW)
-		yprop[m] = yy
-		if(k == 1) y[m] = yy end 
-		ll =  likelixcirc(((m-1)/M)*T, (m/M)*T, v, yy, b, a, B, beta, lambda)
-		llold = likelixcirc(((m-1)/M)*T, (m/M)*T, v, y[m], b, a, B, beta, lambda)
-		#
-		#println(ll)
-		#readline(STDIN)
-		if (llold > 0)
-			acc = min(1.0, ll/llold)
+		Tmax = m/M*TT 
+		Tmin = (m-1)/M*TT
+		if example == "brown"; B = bB*0.01;  end
+		if example == "lin"; B = exp(-0.2*Tmin)*bB; end
+		
+		T = Tmax - Tmin
+		Ts =  linspace(0, T-dt, n)
+		lambda = zero(a(Tmax,v))
+
+		try
+			lambda = Lyap.lyap(B', -a(Tmax,v))
+		end
+		#print(lambda)
+		Smax = LinProc.taui(T-2*dt,T)
+		S = linspace(0.0,Smax, n)
+
+		#S = -log(1.-Ts/T) 
+		Ds = diff(S)
+
+	 	DW = randn(2,n-1).*[sqrt(Ds)  sqrt(Ds)]'
+		
+		u0 = LinProc.UofX(0.0,u,  T, v,  B, beta)
+				
+	 	U = LinProc.eulerv(0.0, u0, LinProc.bU(Tmin, T, v, b, a, B, beta, lambda), (s,x) -> sigma(LinProc.ddd(s,x, Tmin, T, v,  B, beta)...), Ds, sqrt(T)*DW)
+	
+		ll[m] = exp(LinProc.llikeliU(S, U, Tmin, T, v, b, a,  B, beta, lambda))
+		time, Y = LinProc.XofU2(S, U,  Tmin, T, v,  B, beta) 
+	 	yprop[m] = Y
+	 	if(k == 1) 
+			y[m] = Y
+			llold[m] = ll[m]
+		end 
+		
+		if (llold[m] > 0)
+			acc = min(1.0, ll[m]/llold[m])
 		else
 			acc = 1.0
 		end
-		println("\t acc ", round(acc, 3), " ", round(llold,3), " ", round(ll,3), " ", round(acc,3))
+		println("\t acc ", round(acc, 3), " ", round(llold[m],7), " ", round(ll[m],7), " ", round(acc,3))
 	
 		if (rand() <= acc)
-			  bb += 1
-			  y[m] = yy
+			  bb[m] += 1
+			  y[m] = Y
+			  llold[m] = ll[m]
 			 
 		end	
-			
-
-	end
-	p = plstep(xtrue, xd, y, yprop )
-	if(SV && k > K/2) Winston.file(p, "img/s$(si)a$(alpha)k$k.png") end
-	mu = 0.
-	W = 1./s^2
 		
-	for m = 1: M
-		mu += mu0(y[m], Dt)   
-		W  += W0(y[m], Dt)     
+		if (k < K/2) llmax = max(llmax, ll) end
+	
+		p[m] = plstep(p[m], xtrue, xd, yprop, ll, llmax,  m)
+	
+
 	end
-	#
-	#th = mu/W + norm(1)*sqrt(1/W)	
-	th=th
-	thetas[k] = th
+	println("\t",round(llmax,3))
 
-	println("k $k\tth $th acc ", round(100*bb/k/M,2 ))
+ 	Winston.display(p[1])
+
+		
+
+
+
+	println("k $k\t acc ",  round(100*mean(bb)/k,2), round(100*bb/k,2), )
+
 end
+if(SV)
+for m = 1:M; Winston.file(p[m], "img/$example$m.png") end 
+end 
 
-thetas2 = thetas[(K/2):end]
-mi = round(mean(thetas2), 3)
-ci = round(1.96*std(thetas2)/sqrt(length(thetas2)),3)
-println("theta = [", mi-ci, ", ", mi + ci, "] (95%-ci)")
 
-println("bridge acc %", round(100*bb/K/M,2))
-
+println("bridge acc %",  round(100*mean(bb)/K,2), round(100*bb/K,2), )
+#Winston.display(p[1])
