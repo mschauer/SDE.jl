6a9cf76da6db1a443bd2e113bbc7eeb8c9857184
M. Schauer <moritzschauer@web.de>
2013-09-04 15:07:41 +0200

    SDE.jl: (m).

--- src/SDE.jl
+++ src/SDE.jl
@@ -1,6 +1,6 @@
 include(Pkg.dir("SDE")*"/src/Diffusion.jl")
 include(Pkg.dir("SDE")*"/src/Schauder.jl")
-include(Pkg.dir("SDE")*"/src/SDENonparBayes.jl")
+include(Pkg.dir("SDE")*"/src/NonparBayes.jl")
 include(Pkg.dir("SDE")*"/src/Randm.jl") 
 include(Pkg.dir("SDE")*"/src/LinProc.jl")
 include(Pkg.dir("SDE")*"/src/Lyap.jl")
