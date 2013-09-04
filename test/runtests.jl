2ca1e1657ca42dfac28892352ed0d99135c20fd6
M. Schauer <moritzschauer@web.de>
2013-09-05 01:06:27 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ test_type = length(ARGS) == 1 ? ARGS[1] : "TEST"
 include("testdiff.jl")
 include("testsch.jl")
 include("testlyap.jl")
-#include("testnonpar.jl")
+include("testnonpar.jl")
 
 if test_type == "ALL"
 	include("testlinproc.jl")
