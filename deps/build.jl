ad9536708638a578cb43d63e1e3e61b1de7f7556
mschauer <moritzschauer@web.de>
2013-09-05 14:39:15 +0200

    build.jl: add Windows build.

--- deps/build.jl
+++ deps/build.jl
@@ -1,5 +1,11 @@
 if OS_NAME == :Windows
-	println("Windows: skipping binary deps, using julia fallback ...")
+	println("Compiling sigma.c...")
+	wd = pwd()
+	cd(dirname(Base.source_path()))
+	run(`gcc -fPIC -O3 -c sigma.c`)
+	println("Linking libsigma...")
+	run(`gcc -shared sigma.o -o libsigma.dll`)
+	cd(wd)
 elseif OS_NAME == :Darwin
 	println("Darwin: skipping binary deps, using julia fallback ...")
 else
